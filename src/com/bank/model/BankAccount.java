01e8755ccaa2c33b765b184435204a4d0c1f355e
HusniddinTsutomu <fakhriddinovdev@gmail.com>
2026-09-01 08:14:26 +0500

    made some changes on project's structure and created exceptions directory for custom exceptions

---
+++ src/com/bank/model/BankAccount.java
@@ -0,0 +1,18 @@
+package src.com.bank.model;
+import java.math.BigDecimal;
+
+
+public class BankAccount {
+    private int accountNumber;
+    private String ownerName;
+    private BigDecimal balance;
+
+    public BankAccount(int accountNumber, String ownerName, BigDecimal balance){
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    // there will be some methods which lets account owner do some actions
+
+}
