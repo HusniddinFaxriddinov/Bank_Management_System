c05d5ce6e58f57d5fa2a2b500cdf4c4a671b1cbd
HusniddinTsutomu <fakhriddinovdev@gmail.com>
2026-09-01 12:35:45 +0500

    add accountID and phoneNumber for user

--- src/com/bank/model/BankAccount.java
+++ src/com/bank/model/BankAccount.java
@@ -1,19 +1,23 @@
 package src.com.bank.model;
 import src.com.bank.exceptions.InsufficientAmountException;
 import src.com.bank.exceptions.InvalidAmountException;
-
+import java.util.Random;
 import java.math.BigDecimal;
 
 
 public class BankAccount {
-    private int accountNumber;
+    private int accountID = 0;
     private String ownerName;
+    private String phoneNumber;
     private BigDecimal balance;
+    private Random rd = new Random();
 
-    public BankAccount(int accountNumber, String ownerName, BigDecimal balance){
-        this.accountNumber = accountNumber;
+
+    public BankAccount(String phoneNumber, String ownerName, BigDecimal balance) {
+        this.phoneNumber = phoneNumber;
         this.ownerName = ownerName;
         this.balance = balance;
+        this.accountID = rd.nextInt(000, 999);
     }
 
     // method for Deposit
@@ -52,4 +56,20 @@ public class BankAccount {
     public BigDecimal getBalance(){
         return balance;
     }
+
+    public int getAccountID(){
+        return accountID;
+    }
+
+    public void setOwnerName(String name){
+        this.ownerName = name;
+    }
+    public String getOwnerName(){
+        return ownerName;
+    }
+
+    public void setPhoneNumber(String phone){
+        this.phoneNumber = phone;
+    }
+    public String getPhoneNumber(){return this.phoneNumber;}
 }
